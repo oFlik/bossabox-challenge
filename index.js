2d0ec596729dd3d6d7dd2b0fa2a024c21ed95cd8
Thiago Pereira <iamthiago2@gmail.com>
2024-04-26 17:47:35 -0300

    Changed server port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const http = require('http');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
