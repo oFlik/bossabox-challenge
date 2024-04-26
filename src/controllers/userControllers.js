eebf0710399c63797c60c4aad2c1bcdc892d17d8
Thiago Pereira <iamthiago2@gmail.com>
2024-04-26 19:22:55 -0300

    Started working on user registration

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,10 +1,30 @@
 const { User } = require('../models/userModel.js');
 
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.find({ email });
+
+    if (userExists) {
+      return res.status(403).send({ message: 'Email já existe! Teste efetuar o login.' });
+    }
+
+    
+
+    return res.status(201).send(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).send({ message: 'Erro do servidor' });
+  }
+};
+
 exports.findAll = async (req, res) => {
   try {
     const users = await User.find({});
     return res.send(users);
   } catch (e) {
-    return res.status(500).send({ message: 'Erro do servidor' });
+    console.log(e.message);
+    res.status(500).send({ message: 'Erro do servidor' });
   }
 };
